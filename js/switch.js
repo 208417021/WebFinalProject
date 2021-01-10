2d08add2ac2fd28d5e9eb2f71df79aa930e3e907
208417021 <208417021@gms.tku.edu.tw>
2021-01-10 20:31:05 +0800

    20210110

--- js/switch.js
+++ js/switch.js
@@ -1,3 +1,14 @@
 function switchPic(picture){
     document.querySelector('.picture').src = picture;
 }
+
+function toggleMenu(){
+    var menuToggle = document.querySelector('.nav-menu');
+    var menuActive = document.querySelector('.active');
+    var navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+    menuToggle.innerHTML = '<i class="fas fa-times fa-2x"></i>';
+    menuActive.innerHTML = '<i class="fas fa-bars fa-2x"></i>';
+}
